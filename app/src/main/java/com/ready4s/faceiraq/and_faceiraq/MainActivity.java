2fc9fc6508bd5e70a662685e6020ef19a5fb232d
psalata <pawel.salata@ready4s.pl>
2017-04-13 17:46:18 +0200

    Initial commit

---
+++ app/src/main/java/com/ready4s/faceiraq/and_faceiraq/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ready4s.faceiraq.and_faceiraq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
