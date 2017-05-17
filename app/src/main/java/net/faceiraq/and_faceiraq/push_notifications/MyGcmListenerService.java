b2ed4b31ebdc9191fe33baa9fa79586906ab2a6a
psalata <pawel.salata@ready4s.pl>
2017-05-17 16:40:35 +0200

    Toast added

--- app/src/main/java/net/faceiraq/and_faceiraq/push_notifications/MyGcmListenerService.java
+++ app/src/main/java/net/faceiraq/and_faceiraq/push_notifications/MyGcmListenerService.java
@@ -2,10 +2,12 @@ package net.faceiraq.and_faceiraq.push_notifications;
 
 import android.app.Service;
 import android.content.Intent;
+import android.icu.text.LocaleDisplayNames;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
@@ -22,5 +24,6 @@ public class MyGcmListenerService extends GcmListenerService {
     public void onMessageReceived(String s, Bundle bundle) {
         super.onMessageReceived(s, bundle);
         Log.d(TAG, "onMessageReceived: ");
+        Toast.makeText(this, "Received push notification", Toast.LENGTH_LONG).show();
     }
 }
