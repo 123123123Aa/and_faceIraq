83c792739235614693a1743c02faeb0d2855ddc7
psalata <pawel.salata@ready4s.pl>
2017-05-17 18:09:52 +0200

    Sratatata

--- app/src/main/java/net/faceiraq/and_faceiraq/push_notifications/MyGcmListenerService.java
+++ app/src/main/java/net/faceiraq/and_faceiraq/push_notifications/MyGcmListenerService.java
@@ -24,6 +24,11 @@ public class MyGcmListenerService extends GcmListenerService {
     public void onMessageReceived(String s, Bundle bundle) {
         super.onMessageReceived(s, bundle);
         Log.d(TAG, "onMessageReceived: ");
-//        Toast.makeText(this, "Received push notification", Toast.LENGTH_LONG).show();
+        try {
+
+        Toast.makeText(this, "Received push notification", Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
